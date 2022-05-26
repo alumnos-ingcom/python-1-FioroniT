3cedad4d2810a5cf9b09f0221bfb804b6dbf1280
Tomás Fioroni <tomasfioroni@gmail.com>
2022-05-26 20:19:15 -0300

    Corrección de argumento 'palabra' --> 'texto'

--- src/ejercicio10.py
+++ src/ejercicio10.py
@@ -2,23 +2,23 @@
 # Fioroni Tomás - @FioroniT
 # UNRN Andina - Introducción a la Ingenieria en Computación
 ################
-def es_palindromo(palabra):
+def es_palindromo(texto):
     """
     En esta función se verifica que la palabra es un palindromo y se devuelve un valor lógico
     """
-    palindromo = palabra[::-1]
-    if palabra == palindromo:
-        palabra = True
+    palindromo = texto[::-1]
+    if texto == palindromo:
+        texto = True
     else:
-        palabra = False
-    return palabra
+        texto = False
+    return texto
 def principal():
     """
     Valor de entrada = palabra (x)
     Valor de salida = valor lógico (True/False)
     """
-    palabra =input("Ingrese una palabra para verificar si es un palindromo: ").lower()
-    print(es_palindromo(palabra))
+    texto = input("Ingrese una palabra para verificar si es un palindromo: ").lower()
+    print(es_palindromo(texto))
 
 if __name__ == "__main__":
     principal()
