bcdeb2babf0f0fb754d80635a02eb3d5f8acf019
Tomás Fioroni <tomasfioroni@gmail.com>
2022-05-27 16:48:50 -0300

    Agrego tests de los ejercicios 4 y 7

--- tests/test_ejercicio4.py
+++ tests/test_ejercicio4.py
@@ -2,8 +2,23 @@
 # Fioroni Tomás - @FioroniT
 # UNRN Andina - Introducción a la Ingenieria en Computación
 ################
-#import pytest
+import pytest
 
-#from src.ejercicio4 import suma_lenta
+from src.ejercicio4 import suma_lenta
 
-#esto está para completar, nadie vió nada
+"""
+En este test se va a probar la funcionalidad del programa de suma lenta de dos números
+"""
+def test_suma_lenta_positivos():
+    numero = 4
+    otro_numero = 3
+    resultado = suma_lenta(numero, otro_numero)
+    assert isinstance(resultado, int), "El resultado debe ser un número de tipo entero"
+    assert resultado == 7,"La suma debe hacerse dígito por dígito"
+def test_suma_lenta_negativos():
+    numero = -4
+    otro_numero = -3
+    resultado = suma_lenta(numero, otro_numero)
+    assert isinstance(resultado, int), "El resultado debe ser un número de tipo entero"
+    assert resultado == -7, "La resta debe hacerse dígito por dígito"
+    
