04d9b3a20224dc66a0a4c5bd78d4ac1257a15253
Tomás Fioroni <tomasfioroni@gmail.com>
2022-06-03 16:10:14 -0300

    Corrección del PEP8 para robot del ejercicio 6

--- src/ejercicio6.py
+++ src/ejercicio6.py
@@ -23,6 +23,8 @@ def ordenar_mayor_a_menor(uno, dos, tres):
     elif uno < dos and uno < tres:
         tupla_mayor = (f"{dos}", f"{tres}", f"{uno}")
     return tupla_mayor
+
+
 def ordenar_menor_a_mayor(uno, dos, tres):
     """
     Los datos de entrada y salida deben ser números enteros
@@ -38,9 +40,11 @@ def ordenar_menor_a_mayor(uno, dos, tres):
         tupla_menor = (f"{tres}", f"{dos}", f"{uno}")
     elif uno > dos and uno < tres:
         tupla_menor = (f"{dos}", f"{uno}", f"{tres}")
-    elif uno < dos and dos > tres:   
+    elif uno < dos and dos > tres:
         tupla_menor = (f"{dos}", f"{tres}", f"{uno}")
     return tupla_menor
+
+
 def principal():
     """
     Los datos de entrada y salida deben ser números enteros
@@ -49,7 +53,9 @@ def principal():
     dos = int(input("Inserte número 2: "))
     tres = int(input("Inserte número 3: "))
     print(ordenar_mayor_a_menor(uno, dos, tres))
-    print(ordenar_menor_a_mayor(uno, dos, tres))   
+    print(ordenar_menor_a_mayor(uno, dos, tres))
+
+
 if __name__ == "__main__":
     principal()
 
