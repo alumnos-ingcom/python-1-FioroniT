1f89bed171c17783c1244ee0ffa04c0d56779bfb
Tomás Fioroni <tomasfioroni@gmail.com>
2022-06-03 16:03:12 -0300

    Corrección del PEP8 para robot del ejercicio 4

--- src/ejercicio4.py
+++ src/ejercicio4.py
@@ -2,7 +2,9 @@
 # Fioroni Tomás - @FioroniT
 # UNRN Andina - Introducción a la Ingenieria en Computación
 ################
-
+"""
+Esta función debe sumar dos números indicados de manera lenta
+"""
 def suma_lenta(numero, otro_numero):
     """
     Los datos de entrada y salida deben ser int
@@ -19,13 +21,17 @@ def suma_lenta(numero, otro_numero):
             numero = numero - 1
             print(numero)
     return numero
+
+
 def principal():
     """
     Los valores de  entrada y salida son int
     """
     numero = int(input("Ingrese el primer numero: "))
     otro_numero = int(input("Ingrese el segundo numero: "))
-    suma_lenta(numero, otro_numero) 
+    suma_lenta(numero, otro_numero)
+
+
 if __name__ == "__main__":
     principal()
 
