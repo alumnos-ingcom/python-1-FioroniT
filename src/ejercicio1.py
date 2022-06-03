7458ddfcbdf62dbe6cef89b8dfdaf4717fb76d6e
Tomás Fioroni <tomasfioroni@gmail.com>
2022-06-03 15:48:40 -0300

    Corrección del PEP8 para robot en el ejercicio1

--- src/ejercicio1.py
+++ src/ejercicio1.py
@@ -11,17 +11,23 @@ def centigrados_a_fahrenheit(centigrados):
     """
     resultado = (centigrados * 9/5) + 32
     return resultado
+
+
 def fahrenheit_a_centigrados(fahrenheit):
     resultado = (fahrenheit - 32) * 5/9
     return resultado
+
+
 def principal():
     """
     Los datos de entrada y salida son únicamente 'int'
-    """    
+    """  
     numero_1 = float(input("Añada cantidad de grados a convertir: "))
     resultado_1 = print(f"{numero_1} C° son {centigrados_a_fahrenheit(numero_1)} F°")
     resultado_2 = print(f"{numero_1} F° son {fahrenheit_a_centigrados(numero_1)} C°")
-    return resultado_1, resultado_2   
+    return resultado_1, resultado_2
+
+
 if __name__ == "__main__":
     principal()
-    
+
