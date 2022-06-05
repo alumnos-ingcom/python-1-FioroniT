c53fc1855355253a6377707a773c5197711a2d5b
Tomás Fioroni <tomasfioroni@gmail.com>
2022-06-05 02:47:57 -0300

    Elimino el retorno que sobra y arreglo principal

--- src/ejercicio8.py
+++ src/ejercicio8.py
@@ -10,11 +10,12 @@ def es_primo(numero):
     Los datos de entrada son números y la salida es un valor lógico
     """
     divisor = 2
+    retorno = True
     while divisor < numero:
         if numero % divisor == 0:
-            return False
+            retorno = False
         divisor = divisor + 1
-    return True
+    return retorno
 
 
 def principal():
@@ -23,6 +24,8 @@ def principal():
     """
     numero = int(input("Ingrese un número para verificar si es primo: "))
     es_primo(numero)
+    salida = es_primo(numero)
+    print(salida)
 
 
 if __name__ == "__main__":
